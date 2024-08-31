30724b37efec567510e2827a8f9b3425c7d67c2e
Fercasanov <fernando.casanova1992@outlook.com>
2024-08-31 15:30:28 -0300

    fix : se reescribira si es necesario

--- src/db/helpers/cartsQuerys.js
+++ src/db/helpers/cartsQuerys.js
@@ -1,57 +1,65 @@
-
+import fs from 'fs';
 import { BadRequest } from '../../errors/badRequest.js';
 import { InternalServerError } from '../../errors/internalServerError.js';
-import { handleFileOperation } from './fs.js';
-
-
 
 export const saveCart = async (cart, path, action) => {
     try {
-        await handleFileOperation(path, cart);
+        await fs.promises.access(`${path}/db/carts.json`).catch(async () => {
+            await fs.promises.writeFile(`${path}/db/carts.json`, JSON.stringify([], null, 2));
+        });
+        await fs.promises.writeFile(`${path}/db/carts.json`, JSON.stringify(cart, null, 2));
         return { message: `Se logró ${action} correctamente el carrito.` };
     } catch (error) {
-        throw new InternalServerError(error.message);
+
+        throw new InternalServerError(error.message)
     }
 };
-
 
 export const saveProductInCart = async (cart, path, action) => {
     try {
-        await handleFileOperation(path, cart);
-        return { message: `Se logró ${action} correctamente el producto al carrito` };
+        await fs.promises.access(`${path}/db/carts.json`).catch(async () => {
+            await fs.promises.writeFile(`${path}/db/carts.json`, JSON.stringify([], null, 2));
+        });
+        await fs.promises.writeFile(`${path}/db/carts.json`, JSON.stringify(cart, null, 2));
+
+        return { message: `Se logró ${action} correctamente el el producto al carrito` };
     } catch (error) {
-        throw new InternalServerError(error.message);
+
+        throw new InternalServerError(error.message)
     }
 };
-
 
 export const getAllCarts = async (path) => {
     try {
-        return await handleFileOperation(path);
+        const productJson = JSON.parse(await fs.promises.readFile(`${path}/db/carts.json`, "utf-8"));
+        return productJson
+   
+
     } catch (error) {
-        throw new InternalServerError(error.message);
+        throw new InternalServerError(error.message)
     }
 };
 
-
 export const getCartById = async (id, path) => {
     try {
+
+
         if (!id) {
-            throw new BadRequest("No se ingresó un ID válido");
-        }
-        const idCart = parseInt(id);
-        const carts = await handleFileOperation(path);
+              throw new BadRequest ("No se ingresó un ID válido") ;
+         }
+        const idCart= parseInt(id);
+        const carts = JSON.parse(await fs.promises.readFile(`${path}/db/carts.json`, "utf-8"));
+
         const cart = carts.find(cart => cart.id === idCart);
-
         if (!cart) {
-            throw new BadRequest(`No existe el carrito con id: ${id}`);
+            throw new BadRequest(`No existe el carrito con id: ${id}`)
         }
-
         return cart;
     } catch (error) {
-        if (error instanceof BadRequest) {
-            throw error;
+        if(error instanceof BadRequest){
+            throw error
         }
-        throw new InternalServerError(error.message);
+        throw new InternalServerError(error)
+   
     }
 };
