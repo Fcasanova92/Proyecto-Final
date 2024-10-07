b8c5c4c4b72d93e5f5f333784f16fec3ad11d4a7
Fercasanov <fernando.casanova1992@outlook.com>
2024-10-07 20:19:47 -0300

    fix : se agregaron las rutas que faltan

--- src/routes/carts/routes.js
+++ src/routes/carts/routes.js
@@ -43,3 +43,47 @@ router.patch("/:cid/products/:pid", async (req, res, next) => {
         next(error)
     }
 });
+
+// eliminar los productos del carrito
+
+router.delete("/:cid/products/:pid", async (req, res, next) => {
+    try {
+        const createCart = await cart.addCart();
+        return res.status(200).json({message:createCart.message});
+        
+    } catch (error) {
+        next(error)
+    }
+});
+
+
+
+// elimina el carrito completo
+
+router.delete("/:cid", async (req, res, next) => {
+    try {
+        const createCart = await cart.addCart();
+        return res.status(200).json({message:createCart.message});
+        
+    } catch (error) {
+        next(error)
+    }
+});
+
+// endpoint para actualizar la cantidad del producto del carrito, debe de ser cambiar la cantidad dentro de la visual del carrito
+
+router.put("/:cid/products/:pid", async (req, res, next) => {
+    try {
+        const idProduct = parseInt(req.params.pid);
+   
+        const idCart = parseInt(req.params.cid);
+ 
+        const cartUpdate = await cart.addProductToCart(idProduct, idCart);
+        
+        return res.status(200).json({ message: cartUpdate.message });
+
+
+    } catch (error) {
+        next(error)
+    }
+});
