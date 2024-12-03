01ff533df363f7a189a900d6cc3bfc441e94cfd8
Fercasanov <fernando.casanova1992@outlook.com>
2024-12-03 20:15:55 -0300

    fix()  : se agrega passportCall y las funciones para conectar el login y el register con el backend

--- src/views/routes/routes.js
+++ src/views/routes/routes.js
@@ -26,7 +26,7 @@ router.get('/', async (req, res, next) => {
       })
       .render('home', {
         title: 'Lista de Productos',
-        productsData,
+        products,
       });
   } catch (error) {
     next('Error al cargar productos');
